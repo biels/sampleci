8e17468b93dcdd364a0a40771a7a44d3d1c29fa3
Biel Simon Rojas <bielsimon2@gmail.com>
2016-12-27 17:50:42 +0100

    Initial commit

---
+++ src/main/java/com/biel/sampleci/SampleciApplication.java
@@ -0,0 +1,12 @@
+package com.biel.sampleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleciApplication.class, args);
+	}
+}
